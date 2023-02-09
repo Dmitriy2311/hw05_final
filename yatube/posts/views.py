0017643e385260c53cec68c3a4cb0df99b56d6d6
Дмитрий Патрушев <diman_patr@mail.ru>
2023-02-09 23:08:13 +0300

    V6

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -39,10 +39,12 @@ def profile(request, username):
     author = get_object_or_404(User, username=username)
     post_list = author.posts.all()
     page_obj = paginat(request, post_list)
-    following = request.user.is_authenticated and Follow.objects.filter(
+    following = (request.user.is_authenticated and
+       request.user != author and
+       Follow.objects.filter(
         user=request.user,
         author=author
-    ).exists()
+    ).exists())
 
     context = {
         'author': author,
